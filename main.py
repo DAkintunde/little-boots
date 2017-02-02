45bc40d71cfdd27265f4f7281b085462ae3fed71
AdemBay <adembayraktar@icloud.com>
2017-02-02 11:36:00 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -5,20 +5,23 @@ import time
 def bufferExtract(buff):
 	return buff[4:5]
 
-i2c = I2C(scl = Pin(5), sda = Pin(4), freq = 100000) #set up i2c crap
+i2c = I2C(scl = Pin(5), sda = Pin(4), freq = 100000) #set up i2c
 
 i2c.writeto_mem(24,0x20,b'\x47') #tell the device to do something (i hate u so much)
 time.sleep_ms(100)
 
 while True:  #print a feed with all the measurements
-	data = ''
-	for i in range(0x28, 0x2e):
-		data += str(i2c.readfrom_mem(24,i,1)) + '\t'
-
-	print(data)
+	for i in range(0x28, 0x2d, 2):
+		data1 = i2c.readfrom_mem(24, i, 1)
+		data2 = i2c.readfrom_mem(24, i+1, 1)
+		value = (int(data2[0] & 0x7f) << 2) + (int(data1[0]) >> 6)
+		#if (0x80 & data2[0] == 0x80):
+		#	value = int(0xfe00 | value) #mmmmMMmMMmm
+		print(bin(value))
+	print()
 	time.sleep(0.1)
 
-"""  #memory dump code, do not delete, was v useful for debugging memory perhaps it should be turned into a function
+"""  #memory dump code, do not delete, was v useful for debugging memory perhaps it should be turned into a functio
 for i in range(0x07, 0x3f):
 	data = str(hex(i)) + " " + str(i2c.readfrom_mem(24,i,1))
 	print(data)
