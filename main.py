bf853bd1f7ba55de5f7f2c3f885421dc64416840
AdemBay <adembayraktar@icloud.com>
2017-02-06 16:53:59 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from machine import Pin, I2C
-import time
+import time, network
 
 def getvalues():  #returns list with the xyz components
 	out = []
-	for i in range(0x28, 0x2d, 2): #cycles through memory with data in
+	for i in range(0x28, 0x2d, 2): #cycles through memory with angle data
 		data1 = i2c.readfrom_mem(24, i, 1) #memory access for LSBs
 		data2 = i2c.readfrom_mem(24, i+1, 1) #memory acces for MSBs
 		value = (int(data2[0]) << 2) + (int(data1[0]) >> 6) #concatinate data
@@ -11,21 +11,31 @@ def getvalues():  #returns list with the xyz components
 				value -= 1024
 		out.append(value)
 	return out
-
-#setup i2c and tell accelerometer to be awake
-i2c = I2C(scl = Pin(5), sda = Pin(4), freq = 100000) #set up i2c
-
-i2c.writeto_mem(24,0x20,b'\x47')
-time.sleep_ms(100)
-
-#main running loop
-while True:
+	
+def printvalues():
 	fred = getvalues()
 	axis = ['x','y','z']
 	for i in range(0,3):
 		print('{}\t{}'.format(axis[i],fred[i]))
 	print()
 	time.sleep(0.1)
+	return
+	
+
+#setup i2c and tell accelerometer to be awake
+i2c = I2C(scl = Pin(5), sda = Pin(4), freq = 100000)
+
+i2c.writeto_mem(24,0x20,b'\x47')
+time.sleep_ms(100)
+
+#connect to network
+#sta_if = network.WLAN(network.STA_IF); sta_if.active(True)
+#sta_if.scan()
+
+#main running loop
+while True:
+	printvalues()
+
 
 """  #memory dump code, do not delete, was v useful for debugging memory perhaps it should be turned into a functio
 for i in range(0x07, 0x3f):
